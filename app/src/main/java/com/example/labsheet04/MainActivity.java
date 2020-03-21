e49458c42ad49cf2a6a359b9397bbc5225694687
udith <udithmadushan2@gmail.com>
2020-03-21 19:35:54 +0530

    Third Commit

--- app/src/main/java/com/example/labsheet04/MainActivity.java
+++ app/src/main/java/com/example/labsheet04/MainActivity.java
@@ -1,56 +1,77 @@
 package com.example.labsheet04;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = String.valueOf(0);
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button =(Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                OpenActivity2();
-            }
-        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = getString(R.string.app_name);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description =
-                    getString(R.string.channel_description);
+            String description = getString(R.string.app_name);
+
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new
-                    NotificationChannel(CHANNEL_ID, name, importance);
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+
             channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager =
-                    getSystemService(NotificationManager.class);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
             notificationManager.createNotificationChannel(channel);
+
         }
 
-    }
-    public void OpenActivity2(){
-        Intent intent= new Intent(this, MainActivity2.class);
-        startActivity(intent);
-    }
-    public void displayToast(View v){
-        Toast.makeText(MainActivity.this,"Hello! ",Toast.LENGTH_SHORT).show();
 
     }
+
+    public void SIGNUP(View view){
+        EditText text =(EditText) findViewById(R.id.editText);
+        String message = text.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Notification")
+                .setContentText("Hello  " +message+ " Welcome to MAD Team" )
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                ;
+
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build());
+
+    }
+
+
+
 }
